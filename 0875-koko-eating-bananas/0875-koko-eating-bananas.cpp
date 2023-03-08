87693a9b3949de285f5df0ed9d7ddc41ad6ecb82
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2023-03-08 18:06:30 +0530

    Time: 42 ms (88.59%), Space: 18.8 MB (82.50%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    #define ll long long
+    bool aukadmehai(vector<int>&p,ll m,ll h)
+    {
+        ll time=0;
+        for(auto &x:p)
+        {
+                ll tmp=x/m;
+                time+=tmp;
+                if(x%m!=0)time++;
+        }
+        return time<=h;
+    }
+    int minEatingSpeed(vector<int>& p, int h) {
+        int l=1;
+        int r=*max_element(p.begin(),p.end());
+        while(l<=r)
+        {
+            int m=l+(r-l)/2;
+            
+            if(aukadmehai(p,m,h))r=m-1;
+            else l=m+1;
+        }
+        return l;
+    }
+};
