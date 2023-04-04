6253469e09297bac3a3ba6e1340a72d9f83f3325
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2023-04-04 12:12:19 +0530

    Time: 78 ms (35.49%), Space: 48.5 MB (51.24%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& a) {
+         // Greddy Approach
+        int n= a.size();
+        int maxIdx=0;
+        for(int i=0;i<=maxIdx;i++)
+        {
+            maxIdx=max(maxIdx,a[i]+i);
+            if(maxIdx>=n-1)return true;
+        }
+        return false;
+    }
+};
