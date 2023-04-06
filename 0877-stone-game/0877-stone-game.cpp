9c272de19ca935eaf066cafc28a93ff7d488e44e
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2023-04-06 23:07:05 +0530

    Time: 3 ms (64.61%), Space: 7.3 MB (96.50%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& p) {
+        //as the total sum is odd so odd idxs and even idxs sum are not equal 
+        //so it all depends on alice that he choose odd or even idxs so he always wins
+        return true;
+    }
+};
