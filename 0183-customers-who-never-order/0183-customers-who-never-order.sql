5f893beaca1898cb0ebccfe842e5373d2aaa0f27
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2023-04-07 18:52:13 +0530

    Time: 891 ms (84.42%), Space: 0B (100.00%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -4,5 +4,9 @@
 # where not exists (select * from Orders o where o.customerId=c.id);
 #upar wala accept hona chahie but usme headers name aara but isko chahie headers me Customers so aisa krna pda alias
 
-select c.Name as Customers from Customers c
-where not exists (select * from Orders o where o.customerId=c.id)
+# select c.Name as Customers from Customers c
+# where not exists (select * from Orders o where o.customerId=c.id)
+
+#one more using left join
+select c.Name as Customers from Customers c 
+left join Orders o on c.Id=o.CustomerId where o.CustomerId is null;
