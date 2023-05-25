ad6f5c558f4b921058a6d7b21ec363c2467c83c8
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2023-05-26 00:22:42 +0530

    Time: 18 ms (36.37%), Space: 20.9 MB (24.09%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -3,7 +3,7 @@ public:
     int solve(vector<int>& sat,int idx,int time,vector<vector<int>>&dp)
     {
         if(idx==sat.size())return 0;
-        
+         
         if(dp[idx][time]!=-1)return dp[idx][time];
         
         int include=sat[idx]*(time+1)+solve(sat,idx+1,time+1,dp);
@@ -12,10 +12,29 @@ public:
         dp[idx][time] = max(include,exclude);
         return dp[idx][time];
     }
-    int maxSatisfaction(vector<int>& sat) {
+    
+    int solveTab(vector<int>& sat)
+    {
         int n=sat.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        
+        for(int idx = n-1; idx>=0 ; idx--)
+        {
+            for(int time = idx ; time >=0 ; time--)
+            {
+                int in = sat[idx]*(time+1)+dp[idx+1][time+1];
+                int ex = 0                +dp[idx+1][time];
+                
+                dp[idx][time] = max(in,ex);
+            }
+        }
+        
+        return dp[0][0];
+    }
+    int maxSatisfaction(vector<int>& sat) {
+        // int n=sat.size();
         sort(sat.begin(),sat.end());
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return solve(sat,0,0,dp);
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return solveTab(sat);
     }
 };
