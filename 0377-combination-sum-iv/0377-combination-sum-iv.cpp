929da136b07f766bbe8f6fbe69cc20913df94233
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2023-05-29 15:59:50 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (89.45%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    int tabuation(vector<int>& nums, int target)
+    int solveMem(vector<int>& nums, int tar,vector<int>& dp)
     {
-        vector<long long>dp(1001,0);
-        dp[0]=1;
+        if(tar<0)return 0;
+        if(tar==0)return 1;
         
-        for(int i=1;i<=target;i++)
+        if(dp[tar] != -1)return dp[tar];
+        
+        int ans = 0;
+        for(int i=0;i<nums.size();i++)
         {
-            for(int j=0;j<nums.size() ;j++)
-            {
-                if(i-nums[j]>=0)
-                {
-                    dp[i]+=dp[i-nums[j]];    
-                    if(dp[i]>=INT_MAX)
-                    {
-                        dp[i] = INT_MAX;
-                        break;
-                    }
-                }
-            }
+            ans+=solveMem(nums,tar-nums[i],dp);
         }
-        return dp[target];
+        return dp[tar]=ans;
     }
-    
-    int combinationSum4(vector<int>& nums, int target) {
-        return tabuation(nums,target);
+    int solve(vector<int>& nums, int tar)
+    {
+        if(tar<0)return 0;
+        if(tar==0)return 1;
+        
+        int ans = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            ans+=solve(nums,tar-nums[i]);
+        }
+        return ans;
+    }
+    int combinationSum4(vector<int>& nums, int tar) {
+        vector<int>dp(tar+1,-1);
+        return solveMem(nums,tar,dp);
     }
 };
