d709786d8f639cb9e726980dc9fccd0ae8cf4c52
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2023-05-29 16:12:58 +0530

    Time: 6 ms (13.28%), Space: 6.6 MB (89.45%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,4 +1,5 @@
 class Solution {
+    #define ll long long 
 public:
     int solveMem(vector<int>& nums, int tar,vector<int>& dp)
     {
@@ -26,8 +27,29 @@ public:
         }
         return ans;
     }
+    
+    int solveTab(vector<int>& nums, int& tar)
+    {
+        vector<ll>dp(tar+1,0);
+        dp[0]=1;
+        
+        for(int i=1;i<=tar;i++)
+        {
+            for(int j=0;j<nums.size();j++)
+            {
+                if(i-nums[j]>=0)
+                {
+                    dp[i] += dp[i-nums[j]];
+                }
+                if (dp[i] > INT_MAX) break; //
+            }
+        }
+        return dp[tar];
+    }
+    
     int combinationSum4(vector<int>& nums, int tar) {
-        vector<int>dp(tar+1,-1);
-        return solveMem(nums,tar,dp);
+        // vector<int>dp(tar+1,-1);
+        // return solveMem(nums,tar,dp);
+        return solveTab(nums,tar);
     }
 };
