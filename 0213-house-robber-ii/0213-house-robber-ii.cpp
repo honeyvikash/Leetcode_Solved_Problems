4498723aaf20cb96b355bfd84749441e78e55da3
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2023-06-01 00:29:32 +0530

    Time: 6 ms (17.10%), Space: 8.1 MB (16.86%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -15,8 +15,31 @@ public:
         return dp[n]=max(include,exclude);
     }
     
+    int solveTab(vector<int>& a)
+    {
+        int n = a.size();
+        vector<int>dp(n,0);
+        dp[0] = a[0];
+        for(int i=1;i<n;i++)
+        {
+            int in = a[i];
+            if(i>1)
+            {
+                 in = dp[i-2]+a[i];
+            }
+            
+            int ex = dp[i-1]+0;
+            dp[i] = max(in,ex);
+        }
+        return dp[n-1];
+    }
+    
     int rob(vector<int>& a) {
+        if(a.empty()) return 0;
+        if(a.size() == 1) return a[0];
+        
         vector<int>first,second;
+        
         for(int i=0;i<a.size();i++)
         {
             if(i==0)
@@ -37,11 +60,11 @@ public:
         int n1 = first.size();
         int n2 = second.size();
         
-        vector<int>dp1(n1+1,-1);
-        vector<int>dp2(n2+1,-1);
+        // vector<int>dp1(n1+1,-1);
+        // vector<int>dp2(n2+1,-1);
         
-        int ans1 = solve(first,n1-1,dp1);
-        int ans2 = solve(second,n2-1,dp2);
+        int ans1 = solveTab(first);
+        int ans2 = solveTab(second);
         return max(ans1,ans2);
     }
 };
