ad12d9e0eae6f0a1192f664d93b1f47e5df42535
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2023-06-08 18:25:37 +0530

    Time: 92 ms (49.63%), Space: 34.4 MB (62.53%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        int ans = 0;
+        map<char,int> m;
+        int cnt = 0;
+        for(auto &x:tasks)
+        {
+            m[x]++;
+            cnt = max(cnt,m[x]);
+        }
+        ans = (cnt-1)*(n+1);
+        for(auto &x:m)
+        {
+            if(x.second==cnt)
+            ans++;
+        }
+        return max((int)tasks.size(),ans);
+    }
+};
