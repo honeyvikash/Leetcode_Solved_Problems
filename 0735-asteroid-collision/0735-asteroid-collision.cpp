910db96e0fb70214ffb4435e634940c6d962991c
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2023-07-19 15:02:39 +0530

    Time: 17 ms (48.80%), Space: 17.2 MB (99.01%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& a) {
+        int n = a.size();
+        vector<int>s;
+        for(int i =0;i<n;i++)
+        {
+            if(s.size()==0 || a[i]>0)
+            {
+                s.push_back(a[i]);
+            }
+            else
+            {
+                while(!s.empty() and s.back()>0 and s.back()<abs(a[i]))s.pop_back();
+                
+                if(!s.empty() and s.back()+a[i]==0)
+                {
+                    s.pop_back();
+                }
+                else if(s.empty() || s.back()<0)s.push_back(a[i]);
+            }
+        }
+        return s;
+    }
+};
