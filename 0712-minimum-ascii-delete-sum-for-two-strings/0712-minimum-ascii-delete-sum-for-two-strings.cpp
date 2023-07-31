f1b20a97ee8f55bd3bd90852aff766a2ffd5ebb9
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2023-07-31 20:25:18 +0530

    Time: 10 ms (99.66%), Space: 7 MB (87.50%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -36,34 +36,34 @@ public:
         
         int n1 = s1.size();
         int n2 = s2.size();
-        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
-        return solve(n1-1,n2-1,s1,s2,dp);
+        // vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+        // return solve(n1-1,n2-1,s1,s2,dp);
         
-//         int dp[n1+1][n2+1];
+        int dp[n1+1][n2+1];
         
-//         for(int i=0;i<n1+1;i++)
-//             dp[i][0] = 0;
-//         for(int i=0;i<n2+1;i++)
-//             dp[0][i] = 0;
-//         for(int i=1;i<n1+1;i++)
-//         {
-//             for(int j=1;j<n2+1;j++)
-//             {
-//                 if(s1[i-1]==s2[j-1])
-//                 {
-//                     dp[i][j] = dp[i-1][j-1]+s1[i-1];
-//                 }
-//                 else
-//                 {
-//                     dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-//                 }
-//             }
-//         }
-//         int sum1=0,sum2=0;
-//         for(auto &i:s1)
-//             sum1+=(i);
-//         for(auto &i:s2)
-//             sum2+=(i);
-//         return (sum1-dp[n1][n2])+(sum2-dp[n1][n2]);
+        for(int i=0;i<n1+1;i++)
+            dp[i][0] = 0;
+        for(int i=0;i<n2+1;i++)
+            dp[0][i] = 0;
+        for(int i=1;i<n1+1;i++)
+        {
+            for(int j=1;j<n2+1;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                    dp[i][j] = dp[i-1][j-1]+s1[i-1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        int sum1=0,sum2=0;
+        for(auto &i:s1)
+            sum1+=(i);
+        for(auto &i:s2)
+            sum2+=(i);
+        return (sum1-dp[n1][n2])+(sum2-dp[n1][n2]);
     }
 };
