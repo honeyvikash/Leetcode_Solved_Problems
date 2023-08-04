d786cdc22c7c89183b26dc2c4b7f847eee31d804
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2023-08-04 19:37:13 +0530

    Time: 44 ms (97.32%), Space: 48.3 MB (72.38%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
+    bool solve(int i,vector<int>& a,int n,vector<int>&dp)
+    {
+        if(i==n-1)return 1;
+        if(i>=n)return 0;
+        if(dp[i]!=-1)return dp[i];
+        dp[i]=0;
+        for(int jump=1;jump<=a[i];jump++)
+        {
+            dp[i] |= solve(i+jump,a,n,dp);
+        }
+        return dp[i];
+    }
     bool canJump(vector<int>& a) {
-         // Greddy Approach
+        // Greddy Approach
         int n= a.size();
         int maxIdx=0;
         for(int i=0;i<=maxIdx;i++)
         {
             maxIdx=max(maxIdx,a[i]+i);
-            if(maxIdx>=n-1)return true;
+            if(maxIdx>=n-1)return 1;
         }
-        return false;
+        return 0;
+        // vector<int>dp(n+1,-1);
+        // return solve(0,a,a.size(),dp);
     }
 };
