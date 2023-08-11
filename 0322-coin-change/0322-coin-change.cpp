a57b474fdbaded3fd4f2cdd0c7890de6de6f82c1
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2023-08-11 14:14:06 +0530

    Time: 64 ms (90.20%), Space: 10.6 MB (92.47%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
-    
-    int BottomUp(vector<int>& c, int rs)
-    {
-        vector<int>dp(rs+1,INT_MAX);
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        int m = amount;
+        int dp[n+1][m+1];
         
-        dp[0]=0;
-        
-        for(int i=1;i<=rs;i++)
+        for(int i=0;i<n+1;i++)
         {
-            for(int j=0;j<c.size();j++)
+            for(int j=0;j<m+1;j++)
             {
-                if(i-c[j]>=0 && dp[i-c[j]]!=INT_MAX)
+
+                if(j==0)
                 {
-                    dp[i]=min(dp[i],1+dp[i-c[j]]);
+                    dp[i][j] = 0;
+                }
+                if(i==0)
+                {
+                    dp[i][j] = INT_MAX-1;
+                }
+
+            }
+        }
+
+
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<m+1;j++)
+            {
+                if(j>=coins[i-1])
+                {
+                    dp[i][j] = min(dp[i][j-coins[i-1]] + 1, dp[i-1][j]);
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
-        return dp[rs]==INT_MAX?-1:dp[rs];
+
+        if(dp[n][m] == INT_MAX-1) return -1;
+
+        return dp[n][m];
     }
-    
-    int coinChange(vector<int>& c, int rs)
-    {
-        return BottomUp(c,rs);
-    }   
 };
