960e425860642337518a7c6aa0b482d01bbedfc4
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2023-08-19 17:44:55 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (22.02%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
-        int dp[1000][1000];
-        int solve(int i, int j, int m, int n)
+        int solveTab(int m, int n) 
         {
-            if(i==m-1 && j==n-1)
+            vector<vector<int>>dp(m,vector<int>(n,0));
+            dp[0][0] = 1;
+            
+            for(int i=1; i<n; i++)
             {
-                return dp[i][j]=1;
+                dp[0][i] = 1;
             }
-            if(i>=m || j>=n)
+            
+            for(int i=1; i<m; i++)
             {
-                return dp[i][j]=0;
+                dp[i][0] = 1;
             }
 
-            if(dp[i][j]!=-1)return dp[i][j];
-
-            return dp[i][j]=(solve(i+1,j,m,n)+solve(i,j+1,m,n));
+            for(int i=1; i<m; i++) 
+            {
+                for(int j=1; j<n; j++)
+                {
+                    dp[i][j] = dp[i][j-1] + dp[i-1][j];
+                }
+            }
+            return dp[m-1][n-1];
         }
         int uniquePaths(int m, int n) {
-            memset(dp,-1,sizeof(dp));
-            return solve(0,0,n,m);   
+            // memset(dp,-1,sizeof(dp));
+            // return solve(0,0,n,m);
+            return solveTab(m,n);
         }
 };
