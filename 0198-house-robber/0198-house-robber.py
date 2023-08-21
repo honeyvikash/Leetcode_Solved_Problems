badfacd8b0c5019a6dab6b9bd4304f7f07ba2428
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2023-08-22 00:39:09 +0530

    Time: 49 ms (28.42%), Space: 16.3 MB (71.71%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        @lru_cache
+        def rec(i):
+            if i==0:
+                return nums[i]
+            if i<0:
+                return 0
+            pick = nums[i] + rec(i-2)
+            not_pick = 0 + rec(i-1)
+            return max(pick,not_pick)
+        
+        return rec(len(nums)-1)
