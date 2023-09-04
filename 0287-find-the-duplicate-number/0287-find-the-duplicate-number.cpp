1b7026f655ca21339fc8a5c5550c2b3b4e0ce3b2
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2023-09-04 20:29:33 +0530

    Time: 76 ms (88.46%), Space: 61.2 MB (46.60%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& a) {
-        //Linked List cycle detection method
-        while(a[0]!=a[a[0]])swap(a[0],a[a[0]]);
-        return a[0];
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while(fast!=slow)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        fast = nums[0];
+        slow = nums[slow];
+        while(fast!=slow)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
     }
 };
