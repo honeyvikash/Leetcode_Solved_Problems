fb5c689d9a4fb41c92006e036faead5f00f433ce
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2023-09-09 10:16:24 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (46.24%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,6 +1,20 @@
 class Solution {
     #define ll long long 
 public:
+    
+    int solve(vector<int>& nums, int tar)
+    {
+        if(tar<0)return 0;
+        if(tar==0)return 1;
+        
+        int ans = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            ans+=solve(nums,tar-nums[i]);
+        }
+        return ans;
+    }
+    
     int solveMem(vector<int>& nums, int tar,vector<int>& dp)
     {
         if(tar<0)return 0;
@@ -15,41 +29,9 @@ public:
         }
         return dp[tar]=ans;
     }
-    int solve(vector<int>& nums, int tar)
-    {
-        if(tar<0)return 0;
-        if(tar==0)return 1;
-        
-        int ans = 0;
-        for(int i=0;i<nums.size();i++)
-        {
-            ans+=solve(nums,tar-nums[i]);
-        }
-        return ans;
-    }
-    
-    int solveTab(vector<int>& nums, int& tar)
-    {
-        vector<ll>dp(tar+1,0);
-        dp[0]=1;
-        
-        for(int i=1;i<=tar;i++)
-        {
-            for(int j=0;j<nums.size();j++)
-            {
-                if(i-nums[j]>=0)
-                {
-                    dp[i] += dp[i-nums[j]];
-                }
-                if (dp[i] > INT_MAX) break; //
-            }
-        }
-        return dp[tar];
-    }
     
     int combinationSum4(vector<int>& nums, int tar) {
-        // vector<int>dp(tar+1,-1);
-        // return solveMem(nums,tar,dp);
-        return solveTab(nums,tar);
+        vector<int>dp(tar+1,-1);
+        return solveMem(nums,tar,dp);
     }
 };
