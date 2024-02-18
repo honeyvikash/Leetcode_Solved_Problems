5a8bd3f71e758939d0d58a799c186127a6bff017
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2024-02-18 11:15:32 +0530

    Time: 238 ms (7.44%), Space: 130.9 MB (5.14%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
-    int furthestBuilding(vector<int>& arr, int b, int l) {
-        
-        int n = arr.size();
-        priority_queue<long long,vector<long long>,greater<long long>> pq;
-
-        long long sm = 0,ts=0,i;
-        for(i = 0 ; i < n-1 ; ++i)
+    bool possible(int x,vector<int>heights, int bricks, int ladders)
+    {
+        vector<int>adj_difference;
+        for(int i=0;i<x;i++)
         {
-            long long da = arr[i+1]-arr[i];
-            if(da<=0)
-                continue;
-            else
+            if(heights[i+1]>heights[i])
             {
-                ts += da;
-
-                pq.push({da});
-                sm+=da;
-
-                if(pq.size()>l)
-                {
-                    sm-=pq.top();
-                    pq.pop();
-                }
-
-                if(ts-sm>b)
-                {
-                    break;
-                }
+                adj_difference.push_back(heights[i+1]-heights[i]);
             }
         }
-        return i;
-
+        sort(adj_difference.begin(),adj_difference.end(),greater<int>());
+        for(int i=adj_difference.size()-1;i>=0;i--)
+        {
+            bricks-=adj_difference[i];
+            // cout<<bricks<<endl;
+            if(bricks<0)
+            {
+                break;
+            }
+            else
+            {
+                adj_difference.pop_back();
+            }
+        }
+        return adj_difference.size()<=ladders?1:0;
+    }
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        // Binary Search Approach
+        int n = heights.size();
+        int l=0,h=n;
+        while(l<h-1)
+        {
+            int m=l+(h-l)/2;
+            if(possible(m,heights,bricks,ladders)) l =m;
+            else h=m;
+        }
+        return l;
     }
 };
