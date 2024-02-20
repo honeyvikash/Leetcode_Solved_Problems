ba96effa7c8bc2e909408e3d582b89f2e1fb5e92
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2024-02-20 07:52:07 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (62.81%) - LeetHub

--- 3019-number-of-changing-keys/3019-number-of-changing-keys.cpp
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int countKeyChanges(string s) {
+        // int n = s.size(), ans = 0;
+        
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(s[i] >= 'A' && s[i] <= 'Z')
+//                 s[i] = 'a' + s[i] - 'A';
+//         }
+        
+//         for(int i = 1; i < n; i++)
+//         {
+//             if(s[i] != s[i-1]) ans++;
+//         }
+        
+//         return ans;
         int n = s.size(), ans = 0;
-        
-        for(int i = 0; i < n; i++)
-        {
-            if(s[i] >= 'A' && s[i] <= 'Z')
-                s[i] = 'a' + s[i] - 'A';
-        }
-        
-        for(int i = 1; i < n; i++)
-        {
-            if(s[i] != s[i-1]) ans++;
-        }
-        
+        for (int i = 0; i + 1 < n; i++) if (tolower(s[i]) != tolower(s[i+1])) ans++;
         return ans;
     }
 };
