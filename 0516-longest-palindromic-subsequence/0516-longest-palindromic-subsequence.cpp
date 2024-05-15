517f447efe781e67662f49eefb75385ff1932898
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2024-05-15 12:33:02 +0530

    Time: 53 ms (69.55%), Space: 11.8 MB (83.33%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     int longestPalindromeSubseq(string s) {
-        int len = s.size();
-        string rowStr = s,colStr = s;
-        reverse(colStr.begin(),colStr.end());
-        vector<vector<int>> dp(len,vector<int>(len,0));
-        for(int i=0 ; i<len ; i++){
-            for(int j=0 ; j<len ; j++){
-                if( colStr[i]==rowStr[j] ){
-                    int ans = 0;
-                    if( j!=0 )  ans = max(ans,dp[i][j-1]);
-                    if( i!=0 )  ans = max(ans,dp[i-1][j]);
-                    if( i!=0 && j!=0 )  ans = max(ans,dp[i-1][j-1]+1);
-                    else    ans = max(ans,1);
-                    dp[i][j] = ans;
-                }else{
-                    int ans = 0;
-                    if( i!=0 )  ans = dp[i-1][j];
-                    if( j!=0 )  ans = max(ans,dp[i][j-1]);
-                    dp[i][j] = ans;
+        string text1=s;
+        reverse(s.begin(),s.end());
+        string text2=s;
+        int n = text1.size();
+        int m = text2.size();
+        int dp[n+1][m+1];
+        for(int i = 0; i<n+1; ++i)
+        {
+            for(int j = 0; j<m+1; ++j)
+            {
+                if(i*j == 0){
+                    dp[i][j] = 0;
                 }
             }
         }
-        // for(auto &val : dp){
-        //     for(auto v : val)   cout<<v<<" ";
-        //     cout<<"\n";
-        // }
-        return dp.back().back();
+        for(int i = 1; i<n+1; ++i)
+        {
+            for(int j = 1; j<m+1; ++j)
+            {
+                if(text1[i-1] == text2[j-1])
+                {
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
