09d957f88d858eb3e2305fd96ad7810a12de3cc9
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2024-05-18 00:01:29 +0530

    Time: 158 ms (16.42%), Space: 38.5 MB (16.19%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -13,13 +13,16 @@ public:
         bool flag = false;
         int n = a.size();
         
-        for(int i=1;i<=n-1;i++){
-            if(isScramble(a.substr(0,i),b.substr(0,i)) && isScramble(a.substr(i),b.substr(i))){
+        for(int i=1;i<=n-1;i++)
+        {
+            if(isScramble(a.substr(0,i),b.substr(0,i)) && isScramble(a.substr(i),b.substr(i)))
+            {
                 flag = true;
                 break;
             }
         
-            if(isScramble(a.substr(0,i),b.substr(n-i)) && isScramble(a.substr(i),b.substr(0,n-i))){
+            if(isScramble(a.substr(0,i),b.substr(n-i)) && isScramble(a.substr(i),b.substr(0,n-i)))
+            {
                 flag = true;
                 break;
             }
