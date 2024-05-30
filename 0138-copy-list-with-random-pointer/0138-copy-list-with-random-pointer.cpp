d34b20ea5a6690c6603d55bae473ad7698c32551
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2024-05-30 05:44:10 +0530

    Time: 5 ms (65.87%), Space: 13.4 MB (36.70%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -13,62 +13,27 @@ public:
     }
 };
 */
+
 class Solution {
 public:
-    void putDuplicateNodes(Node *head)
-    {
-        while( head )
-        {
-            head->next = new Node(head->val,head->next);
-            head = head->next->next;
-        }
-    }
-    
-    void addRandomPointerToDuplicateNodes(Node *head)
-    {
-        while( head )
-        {
-            if( head->random )
-                head->next->random = head->random->next;
-            else
-                head->next->random = NULL;
-            head = head->next->next;
-        }
-    }
-    
-    Node* giveDuplicateList(Node *head)
-    {
-        if( NULL==head )    return head;
-        Node *cp = head;
-        head = head->next;
-        // a A b B c C d D g G N
-        // cp = c
-        // nc = d
-        // A B C 
-        // a b c
-        while( cp )
-        {
-            Node *nextCp = cp->next->next;
-            if( nextCp && nextCp->next )
-                cp->next->next = nextCp->next;
-            else
-                cp->next->next = NULL;
-            cp->next = nextCp;
-            cp = nextCp;
-        }
-        return head;
-    }
-    
     Node* copyRandomList(Node* head) {
-        putDuplicateNodes(head);
-        addRandomPointerToDuplicateNodes(head);
-        Node *temp = head;
-        // while( temp ){
-        //     cout<<temp->val<<" ";
-        //     if( temp->random )  cout<<temp->random->val;
-        //     cout<<"\n";
-        //     temp = temp->next;
-        // }
-        return giveDuplicateList(head);
+        map<Node *,Node *>mp;
+        Node * temp=head;
+        while(temp!=NULL)
+        {
+            Node * dalo=new Node(temp->val);
+            mp[temp]=dalo;
+            temp=temp->next;
+        }
+        temp=head;
+        Node* ans=mp[temp];
+        while(temp!=NULL)
+        {
+            Node* aage=mp[temp];
+            aage->next=mp[temp->next];
+            aage->random=mp[temp->random];
+            temp=temp->next;
+        }
+        return ans;
     }
 };
