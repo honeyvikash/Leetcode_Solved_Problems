40cd22530c44c8cfdd42a40a50cf9f92bd0b6637
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2024-05-31 22:04:20 +0530

    Time: 5 ms (79.18%), Space: 12.2 MB (98.70%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,5 +1,5 @@
-class Solution {
-public:
+// class Solution {
+// public:
 // intution is like break the array in 2parts such that 1 part contains a, and another part contains b
 // and now there xors will be the answer
 
@@ -9,33 +9,65 @@ public:
 
 // this means that at ith position one of a and b will have 1 and other will have 0;
 // so break the array such that element having 0 at ith in one part and having 1 is in another part
-    vector<int> singleNumber(vector<int>& nums) {
-        int xo = 0;
-        for(int x : nums){
-            xo  = xo^x;
-        }
+    // vector<int> singleNumber(vector<int>& nums) {
+//         int xo = 0;
+//         for(int x : nums){
+//             xo  = xo^x;
+//         }
         
-        //getting the first i where bit is 1;
-        int i;
-        for( i =0;i<32;i++){
-            if( (xo & (1 << i))!=0 ){
+//         //getting the first i where bit is 1;
+//         int i;
+//         for( i =0;i<32;i++){
+//             if( (xo & (1 << i))!=0 ){
+//                 break;
+//             }
+//         }
+//         //x01 is the group which contains all the elements haviong 1 at ith position
+//         //x02 is the group which contains all the elements haviong 0 at ith position
+//         int xo1 =0, xo2=0;
+//         for(auto x : nums){
+//            if( (x & (1 << i))!=0 ){
+//                //this is 1;
+//                xo1 = xo1^x;
+               
+//            }
+//             else{
+//                 //this is 0;
+//                 xo2 = xo2^x;
+//             }
+//         }
+//         return {xo1, xo2};
+//         
+//         
+//     }
+// };
+
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int xorr = nums[0]; 
+        for(int i=1 ; i<nums.size() ; i++)
+        {
+            xorr ^= nums[i] ;
+        }
+
+        int bit = 0 ;
+        while(true)
+        {
+            if((1<<bit)&(xorr))
+            {
                 break;
             }
+             bit++ ;
         }
-        //x01 is the group which contains all the elements haviong 1 at ith position
-        //x02 is the group which contains all the elements haviong 0 at ith position
-        int xo1 =0, xo2=0;
-        for(auto x : nums){
-           if( (x & (1 << i))!=0 ){
-               //this is 1;
-               xo1 = xo1^x;
-               
-           }
-            else{
-                //this is 0;
-                xo2 = xo2^x;
-            }
+
+        int a=0 , b=0 ;
+        for(int i=0 ; i<nums.size() ; i++)
+        {
+            if(nums[i]&(1<<bit)) a^=nums[i] ;
+            else b^=nums[i];
         }
-        return {xo1, xo2};
+
+        return {a,b} ;
     }
 };
