20a50b2a870392883203f2b23d01b49ef811e837
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2024-06-02 01:19:29 +0530

    Time: 11 ms (13.01%), Space: 11.8 MB (82.22%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& a) {
-       //bhaad me jao que
-        unordered_map<int,int>m;
-        for(auto x:a)m[x]++;
-        for(auto x:m)if(x.second==1)return x.first;
-        return -1;
+    int singleNumber(vector<int>& nums) {
+        vector<int>arr(32,0) ;
+        int ans = 0 ;
+        
+            for(int j=0 ; j<=31 ; j++)
+            {
+                int mask = 1<<j;
+                int cnt = 0 ; 
+                for(int i=0 ; i<nums.size() ; i++)
+                {
+                    if(nums[i] & mask) cnt++;
+                }
+                
+                if(cnt%3==1)
+                {
+                    ans = ans | mask ;
+                }
+            }
+
+        return ans ;
+         
     }
 };
