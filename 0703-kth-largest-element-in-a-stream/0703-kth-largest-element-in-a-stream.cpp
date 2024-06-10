766f88788b247e3400f437c8f3884d2d3c0017e3
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2024-06-11 00:33:22 +0530

    Time: 26 ms (52.87%), Space: 23.5 MB (33.64%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -4,7 +4,7 @@ public:
     priority_queue<int, vector<int>, greater<int>> pq;
     KthLargest(int k, vector<int>& nums) {
         kk=k;
-        for(auto&x:nums)
+        for(auto &x:nums)
         {
             pq.push(x);
             if(pq.size()>k)pq.pop();
