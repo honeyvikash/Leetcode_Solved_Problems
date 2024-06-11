19e542049b733f1bb5c98698a3b4e7cf7ca58e6f
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2024-06-11 22:54:00 +0530

    Time: 236 ms (91.86%), Space: 93.1 MB (87.34%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    static bool comparator(vector<int>&v1,vector<int>&v2)
-    {
-        return v1[1]<v2[1];
-    }
     int findMinArrowShots(vector<vector<int>>& p) {
-        // similar to merge intervals
-        if(p.size()==0)return 0;
-        sort(p.begin(),p.end(),comparator);
-        int cnt=1;
-        int curr=p[0][1];
-        for(int i=1;i<p.size();i++)
+        int n = p.size() ;
+        
+        int cnt = 0 ;
+        sort(p.begin(),p.end()) ;
+        vector<int>cur = p[0] ;
+        for(int i=1 ; i<p.size() ; i++)
         {
-            if(p[i][0]>curr)
+            if(p[i][0]<=cur[1])
             {
-                curr=p[i][1];
-                cnt++;
+                cur[0]=max(cur[0],p[i][0]) ;
+                cur[1]=min(cur[1],p[i][1]) ;
+            }
+            else
+            {
+                cnt++ ;
+                cur = p[i] ;
             }
         }
-        return cnt;
+
+        cnt++ ;
+        return cnt ;
     }
 };
