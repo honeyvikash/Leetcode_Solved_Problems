2238d852e604631c4f743e955f6d65822de13e13
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2024-06-20 14:32:17 +0530

    Time: 8 ms (33.39%), Space: 20.2 MB (11.83%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,42 +1,27 @@
-// class Solution {
-//     int solve(vector<int> &prices,int ind,int buy,vector<vector<int>> &dp){ 
-//     if(ind==prices.size())return 0;
-//     int profit=0;
-//     if(dp[ind][buy]!=-1)return dp[ind][buy];
-//     if(buy)
-//     {
-//         //buy or not buy
-//         int take=-prices[ind]+solve(prices,ind+1,0,dp);
-//         int notTake=solve(prices,ind+1,1,dp);
-//         profit=max(take,notTake);
-//     }
-//     else
-//     {
-//         //sell or not sell
-//         int sell=prices[ind]+solve(prices,ind+1,1,dp);
-//         int notSell=solve(prices,ind+1,0,dp);
-//         profit=max(sell,notSell);
-//     }
-//     return dp[ind][buy]=profit;
-//     }
-//     public:
-//     int maxProfit(vector<int>& prices) {
-//         vector<vector<int>> dp(prices.size(),vector<int> (2,-1));
-//         return solve(prices,0,1,dp);
-//     }
-// };
-
 class Solution {
-public:
+    int solve(vector<int> &prices,int ind,int buy,vector<vector<int>> &dp){ 
+    if(ind==prices.size())return 0;
+    int profit=0;
+    if(dp[ind][buy]!=-1)return dp[ind][buy];
+    if(buy)
+    {
+        //buy or not buy
+        int take=-prices[ind]+solve(prices,ind+1,0,dp);
+        int notTake=solve(prices,ind+1,1,dp);
+        profit=max(take,notTake);
+    }
+    else
+    {
+        //sell or not sell
+        int sell=prices[ind]+solve(prices,ind+1,1,dp);
+        int notSell=solve(prices,ind+1,0,dp);
+        profit=max(sell,notSell);
+    }
+    return dp[ind][buy]=profit;
+    }
+    public:
     int maxProfit(vector<int>& prices) {
-        int ans = 0;
-        for (int i = 1; i < prices.size(); i++) 
-        {
-            if (prices[i] > prices[i - 1]) 
-            {
-                ans += (prices[i] - prices[i - 1]);
-            }
-        }
-        return ans;
+        vector<vector<int>> dp(prices.size(),vector<int> (2,-1));
+        return solve(prices,0,1,dp);
     }
 };
