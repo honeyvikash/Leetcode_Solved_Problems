9db44ebf9dddeb041abc4dd905038bacfbd563ca
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2024-06-22 23:07:38 +0530

    Time: 540 ms (51.83%), Space: 232.3 MB (25.92%) - LeetHub

--- 3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.cpp
+++ 3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.cpp
@@ -5,23 +5,21 @@ public:
         long long ans = 0;
         map<int,int> mp;
         
-        for(auto&a:power)
-        {
+        for(auto&a:power){
             mp[a]++;
         }
         
         vector<array<long long , 2>> pv;
         
-        for(auto &[a,b]:mp)
-        {
+        for(auto&[a,b]:mp){
             pv.push_back({a,b});
         }
         
         int n = pv.size();
         vector<long long> dp(n,0);
         
-        for(int i = 0 ; i < n ; ++i)
-        {   
+        for(int i = 0 ; i < n ; ++i){
+            
             if(i-1>=0 && pv[i-1][0]<pv[i][0]-2)
                 dp[i] = pv[i][0]*pv[i][1] + dp[i-1];
             else if(i-2>=0 && pv[i-2][0]<pv[i][0]-2)
@@ -34,6 +32,8 @@ public:
             if(i!=0)
                 dp[i] = max(dp[i],dp[i-1]);
         }
-        return ans; 
+        
+        return ans;
+        
     }
 };
