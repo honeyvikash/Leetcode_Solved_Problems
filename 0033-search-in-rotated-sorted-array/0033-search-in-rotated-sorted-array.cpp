59bdfae8b5d73f2394e78fcf34c2c75fac6d61c6
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2024-07-09 18:07:40 +0530

    Time: 4 ms (40.31%), Space: 13.5 MB (78.07%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,49 +1,34 @@
 class Solution {
 public:
-    int findingindex(vector<int>& nums)
-    {
-        int n=nums.size();
-        int s=0,e=n-1;
-        if(nums[s]<=nums[e])return 0;
-        while(s<=e)
-        {
-            int m = s + (e - s) / 2;
-            int prev = (m - 1 + n) % n;
-            int next = (m + 1) % n;
-             if (nums[m] <= nums[prev] && nums[m] <= nums[next]) return m;
-            else if (nums[m] <= nums[e])
-                e = m - 1;
-            else if (nums[m] >= nums[s])
-                s = m + 1;
-        }
-        return 0;
-    }
+    int search(vector<int>& nums, int target) {
+        int left = 0 ;
+        int right = nums.size()-1 ;
+        int n = nums.size() ;
 
-    int search(vector<int>& nums, int t) {
-       int i = findingindex(nums);
-        int s1=0,e1=i-1;
-        int s2=i,e2=nums.size()-1;
-        int ans1,ans2;
-        while(s1<=e1)
+        while(left<=right)
         {
-            int m=s1+(e1-s1)/2;
-            if(nums[m]==t)return m;
-            else if(nums[m]<t)
+            int mid = left + (right-left)/2 ;
+
+            if(nums[mid]==target) return mid ;
+
+            if(nums[mid]>=nums[left])
             {
-                s1=m+1;
+                if(target>=nums[left] && target<nums[mid])
+                {
+                    right = mid-1 ;
+                }
+                else left = mid+1 ;
             }
-            else e1=m-1;
-        }
-        while(s2<=e2)
-        {
-            int m=s2+(e2-s2)/2;
-            if(nums[m]==t)return m;
-            else if(nums[m]<t)
+            else
             {
-                s2=m+1;
+                if(target>nums[mid] && target<=nums[right])
+                {
+                    left = mid+1 ;
+                }
+                else right = mid-1 ;
             }
-            else e2=m-1;
         }
-        return -1;
+
+        return -1 ;
     }
 };
