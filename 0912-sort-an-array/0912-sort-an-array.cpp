4d1f055a6710a98dda1f5b2419e0a6dd74238d2b
Vikash Kumar <100141627+honeyvikash@users.noreply.github.com>
2024-07-25 10:56:51 +0530

    Time: 576 ms (25.58%), Space: 284.5 MB (19.25%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,15 +1,50 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& a) {
-        vector<int>ans;
-        priority_queue<int>p;
-        for(auto &x:a)p.push(x);
-        while(!p.empty())
+    void merge(vector<int>&answer, int start, int mid, int end)
+    {
+        vector<int> sorted;
+        int i = start, j= mid+1;
+        while(i<=mid&& j<=end)
         {
-            ans.push_back(p.top());
-            p.pop();
+            if(answer[i]<answer[j])
+            {
+                sorted.push_back(answer[i]);
+                i++;
+            }
+            else
+            {
+                sorted.push_back(answer[j]);
+                j++;
+            }
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        while(i <= mid)
+        {
+            sorted.push_back(answer[i++]);
+        }
+        while(j <= end)
+        {
+            sorted.push_back(answer[j++]);
+        }
+        j=0;
+        for(i=start; i<=end; i++)
+        {
+            answer[i] = sorted[j++] ;
+        }
+    }
+    void mergeSort(vector<int>& nums, int start,int end){
+        if(start >= end)
+        {
+            return;
+        }
+        int mid = start + (end - start)/2 ;
+        mergeSort(nums, start,mid);
+        mergeSort(nums, mid+1, end);
+        merge(nums, start, mid, end);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        int start =0;
+        int end = nums.size()-1;
+        mergeSort(nums,start,end);
+        return nums;
     }
 };
